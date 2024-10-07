d2da8089922b7fe68d1bf042a03b6fb2882c172b
iksshacoder <iks.sha26@gmail.com>
2024-10-07 17:59:26 -0400

    made donate.js

---
+++ static/donate.js
@@ -0,0 +1,9 @@
+
+function SendDonateData(){
+    let nameinput = document.getElementById("full-name").value
+    console.log(nameinput)
+}
+
+
+
+document.getElementById("send").addEventListener("click", SendDonateData)
